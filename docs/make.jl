1832f39237aa735a04f217a48194e1505d0f0de6
UweAlex <deine-email@example.com>
2025-12-14 22:03:40 +0100

    Deploy: Update code and documentation

--- docs/make.jl
+++ docs/make.jl
@@ -5,34 +5,28 @@ DocMeta.setdocmeta!(
     NonlinearOptimizationTestFunctions,
     :DocTestSetup,
     :(using NonlinearOptimizationTestFunctions);
-    recursive=true,
+    recursive = true,
 )
 
 makedocs(
-    sitename="NonlinearOptimizationTestFunctions.jl",
-    authors="Uwe Alex",
-    format=Documenter.HTML(
-        prettyurls=get(ENV, "CI", nothing) == "true",
-        canonical="https://uwealex.github.io/NonlinearOptimizationTestFunctions.jl",
-        assets=String[],
+    sitename = "NonlinearOptimizationTestFunctions.jl",
+    authors = "Uwe Alex",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://uwealex.github.io/NonlinearOptimizationTestFunctions.jl",
+        assets = String[],
     ),
-    modules=[NonlinearOptimizationTestFunctions],
-    pages=[
-        "Home" => "index.md",
-        "Installation" => "installation.md",
-        "Available Test Functions" => "functions.md",
-        "Demos" => "demos.md",
-        "API Reference" => "api.md",
-    ],
-    doctest=true,
-    checkdocs=:exports,
-    warnonly=[:missing_docs, :cross_references],  # Warnt bei Problemen, bricht aber nicht ab
+    modules = [NonlinearOptimizationTestFunctions],
+    pages = [],  # Leer, da keine .md-Dateien mehr existieren – baut nur autogenerierte Docs
+    doctest = true,
+    checkdocs = :exports,
+    warnonly = [:missing_docs, :cross_references],  # Warnt, bricht nicht ab
 )
 
 deploydocs(
-    repo="github.com/UweAlex/NonlinearOptimizationTestFunctions.jl.git",
-    devbranch="master",
-    branch="gh-pages",
-    versions=["stable" => "v^", "v#.#.#", "dev" => "dev"],
-    push_preview=true,
+    repo = "github.com/UweAlex/NonlinearOptimizationTestFunctions.jl.git",
+    devbranch = "master",
+    branch = "gh-pages",
+    versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"],
+    push_preview = true,
 )
